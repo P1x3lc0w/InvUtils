64fe7f7978c17ef68b9433a33417b170a9b8d519
P1x3lc0w <p1x3lc0w@protonmail.com>
2023-04-08 21:11:49 +0200

    Add searchHotbarFirst and enderChestSilkTouch settings

--- InvUtil/src/main/kotlin/p1x3lc0w/invutil/config/AutoToolConfig.kt
+++ InvUtil/src/main/kotlin/p1x3lc0w/invutil/config/AutoToolConfig.kt
@@ -2,6 +2,8 @@ package p1x3lc0w.invutil.config
 
 class AutoToolConfig {
     var prioritizeHigherMiningLevelTools: Boolean = true
+    var searchHotbarFirst: Boolean = true
+    var enderChestSilkTouch: Boolean = true
     var glassSilkTouch: Boolean = true
     var targetSlot: AutoToolTargetSlot = AutoToolTargetSlot.FirstTool
 }
